02c1a3fbe39c9434e4f8fb877f7333b30ba96f7f
shooting_star <shooting_star>
2023-10-18 00:08:29 +0200

    Keybinds and forgotten applications AND Z THE GODLIKE

--- main.sh
+++ main.sh
@@ -5,13 +5,21 @@ yay -S --needed hyprland kitty xdg-desktop-portal-hyprland sddm mako wofi polkit
 yay -S --needed vscodium-bin eww-wayland swww grim slurp
 
 #Optional Stuff
-yay -S --needed obsidian anki fish fisher spotify spicetify-cli discord
+yay -S --needed obsidian anki fish fisher starship spotify-launcher spicetify-cli discord torbrowser-launcher
 
 #Laptop Stuff
+yay -S --needed brightnessctl
 
+cat ./hypr_shooting_keybinds >> ~/.config/hypr/hyprland.conf
 
 chsh -s /usr/bin/fish
 fisher install franciscolourenco/done
+fisher install nickeb96/puffer-fish
+fisher install jethrokuan/z
+
+echo "
+starship init fish | source" >> ~/.config/fish/config.fish
+
 
 echo "--enable-features=WaylandWindowDecorations
 --ozone-platform-hint=auto" > ~/.config/electron25-flags.conf
